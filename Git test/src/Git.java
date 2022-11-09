ecccf83e505649f0dc3bb5756a974816aa0c0368
aiswa <aiswa@Aiswarya>
2022-11-09 11:29:55 +0530

    added git

---
+++ Git test/src/Git.java
@@ -0,0 +1,17 @@
+
+public class Git 
+{
+static int a=10;
+static int b=15;
+static int c=a+b;
+	public static void main(String[] args)
+	{
+	
+// TODO Auto-generated method stub
+System.out.println(a);
+System.out.println(b);
+System.out.println(c);
+
+	}
+
+}
